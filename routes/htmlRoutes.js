ea303f08fd0d380f3853487de5c1ee8588e5a4f5
Sean <sdillon215@gmail.com>
2021-08-28 20:08:45 -0700

    get route for notes page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.get('/', (req, res) => {
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// GET route for homepage and all incorrect paths
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
