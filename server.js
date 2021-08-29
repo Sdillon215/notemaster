892adfd39b2179c9b82d328beb0dc001fa5b62d0
Sean <sdillon215@gmail.com>
2021-08-28 20:03:45 -0700

    server running, get route for index.html

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+app.listen(PORT, () => {
+    console.log(`Server now on port ${PORT}!`);
+});
